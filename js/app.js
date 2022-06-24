7c745b085c5e5b565d653a9604070f282bab2a6b
Jessica <jessicaoritsegbubemi@gmail.com>
2022-06-24 15:00:25 +0100

    added the dark theme functionality

--- js/app.js
+++ js/app.js
@@ -21,3 +21,35 @@ close.addEventListener("click", function () {
 //   });
 // }
 // addActiveToLinks();
+
+// ********************* DARK THEME **************************
+let body = document.querySelector("body");
+const darkTheme = document.querySelector(".comp-img-light");
+const lightTheme = document.querySelector(".comp-img-dark");
+const changeLogo = document.querySelector("header");
+var theme = "";
+
+const changeTheme = function (theme) {
+  if (theme === "dark") {
+    body.classList.add("dark");
+    changeLogo.classList.add("display");
+  } else {
+    body.classList.remove("dark");
+    changeLogo.classList.remove("display");
+  }
+};
+
+darkTheme.addEventListener("click", function () {
+  theme = "dark";
+  changeTheme(theme);
+  localStorage.setItem("class", theme);
+});
+
+lightTheme.addEventListener("click", function () {
+  theme = "";
+  changeTheme(theme);
+  localStorage.setItem("class", "light");
+});
+
+theme = localStorage.getItem("class");
+changeTheme(theme);
